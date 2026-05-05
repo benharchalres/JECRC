2c3ac7a15980a759fdc3a6e126a9fd428cfce1c0
benhar091983 <benharcharles.velankanni@hclguvi.com>
2026-05-05 15:11:40 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FantacyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
